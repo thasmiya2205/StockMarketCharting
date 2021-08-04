a17cf67023ffdd15905e9f0ce21d5579f22ed431
thasmiya2205 <61138709+thasmiya2205@users.noreply.github.com>
2021-08-04 20:14:16 +0530

    initial commit

---
+++ frontend-angular/src/app/LOGout comp/logout.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { LoginService } from 'src/app/services/login.service';
+import { UserService } from 'src/app/services/user.service';
+
+@Component({
+  selector: 'app-logout',
+  templateUrl: './logout.component.html',
+  styleUrls: ['./logout.component.css']
+})
+export class LogoutComponent implements OnInit {
+
+  constructor(private userService:UserService) { }
+
+  ngOnInit(): void {
+    this.userService.logOut()
+  }
+
+}
